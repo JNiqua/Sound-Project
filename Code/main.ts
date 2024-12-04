db1ae821436fac034f88834716d89c642acfb5e9
Jannick Klose <ja.l.klose@gmail.com>
2024-12-04 20:39:17 +0100

    anothertry

--- Code/main.ts
+++ Code/main.ts
@@ -23,37 +23,37 @@ namespace Marketplace {
     
     
     // const pastLoopMusic: HTMLAudioElement = <HTMLAudioElement>document.getElementById("music");
-    const pastLoopMusic: HTMLAudioElement = new Audio("/AudioFiles/Past/MarketMusic_Past.wav");
+    const pastLoopMusic: HTMLAudioElement = new Audio("AudioFiles/Past/MarketMusic_Past.wav");
     // const pastLoop: HTMLAudioElement = <HTMLAudioElement>document.getElementById("main");
-    const pastLoop: HTMLAudioElement = new Audio("/AudioFiles/Past/MainMedieval.wav");
-    const pastGenChurch: HTMLAudioElement = new Audio("/AudioFiles/Past/Church.wav");
-    const pastGenCoins: HTMLAudioElement = new Audio("/AudioFiles/Past/Coins1.wav");
-    const pastGenCrow: HTMLAudioElement = new Audio("/AudioFiles/Past/Crow.wav");
-    const pastGenFight: HTMLAudioElement = new Audio("/AudioFiles/Past/Fight.wav");
-    const pastGenHammering: HTMLAudioElement = new Audio("/AudioFiles/Past/Hammerring.wav");
-    const pastGenHorsecart: HTMLAudioElement = new Audio("/AudioFiles/Past/Horsecart.wav");
-    const pastGenTavern: HTMLAudioElement = new Audio("/AudioFiles/Past/Tavern.wav");
+    const pastLoop: HTMLAudioElement = new Audio("AudioFiles/Past/MainMedieval.wav");
+    const pastGenChurch: HTMLAudioElement = new Audio("AudioFiles/Past/Church.wav");
+    const pastGenCoins: HTMLAudioElement = new Audio("AudioFiles/Past/Coins1.wav");
+    const pastGenCrow: HTMLAudioElement = new Audio("AudioFiles/Past/Crow.wav");
+    const pastGenFight: HTMLAudioElement = new Audio("AudioFiles/Past/Fight.wav");
+    const pastGenHammering: HTMLAudioElement = new Audio("AudioFiles/Past/Hammerring.wav");
+    const pastGenHorsecart: HTMLAudioElement = new Audio("AudioFiles/Past/Horsecart.wav");
+    const pastGenTavern: HTMLAudioElement = new Audio("AudioFiles/Past/Tavern.wav");
     
     // const pastAudio: AudioScene = new Past(pastLoopMusic, pastLoop, pastGenChurch, pastGenCoins, pastGenCrow, pastGenFight, pastGenHammering, pastGenHorsecart, pastGenTavern);
     
     
-    const presentLoopMusic: HTMLAudioElement = new Audio("/AudioFiles/Present/MarketMusic_Present.wav");
-    const presentLoopCrowd: HTMLAudioElement = new Audio("/AudioFiles/Present/crowd_loop.wav");
-    const presentLoopWindleaves: HTMLAudioElement = new Audio("/AudioFiles/Present/windleaves_loop.wav");
-    const presentLoopPigeons: HTMLAudioElement = new Audio("/AudioFiles/Present/pigeon_loop.wav");
-    const presentGenCar: HTMLAudioElement = new Audio("/AudioFiles/Present/car1_stereo.wav");
-    const presentGenTrain: HTMLAudioElement = new Audio("/AudioFiles/Present/train_stereo.wav");
-    const presentGenSuperMarket1: HTMLAudioElement = new Audio("/AudioFiles/Present/supermarket1.wav");
-    const presentGenSuperMarket2: HTMLAudioElement = new Audio("/AudioFiles/Present/supermarket2.wav");
+    const presentLoopMusic: HTMLAudioElement = new Audio("AudioFiles/Present/MarketMusic_Present.wav");
+    const presentLoopCrowd: HTMLAudioElement = new Audio("AudioFiles/Present/crowd_loop.wav");
+    const presentLoopWindleaves: HTMLAudioElement = new Audio("AudioFiles/Present/windleaves_loop.wav");
+    const presentLoopPigeons: HTMLAudioElement = new Audio("AudioFiles/Present/pigeon_loop.wav");
+    const presentGenCar: HTMLAudioElement = new Audio("AudioFiles/Present/car1_stereo.wav");
+    const presentGenTrain: HTMLAudioElement = new Audio("AudioFiles/Present/train_stereo.wav");
+    const presentGenSuperMarket1: HTMLAudioElement = new Audio("AudioFiles/Present/supermarket1.wav");
+    const presentGenSuperMarket2: HTMLAudioElement = new Audio("AudioFiles/Present/supermarket2.wav");
     
     // const presentAudio: AudioScene = new Present(presentLoopMusic, presentLoopCrowd, presentLoopWindleaves, presentLoopPigeons, presentGenCar, presentGenTrain, presentGenSuperMarket1, presentGenSuperMarket2);
     
     
-    const futureLoopMusic: HTMLAudioElement = new Audio("/AudioFiles/Future/MarketMusic_Future.wav");
-    const futureLoop: HTMLAudioElement = new Audio("/AudioFiles/Future/FutureMain.wav");
-    const futureGenFight: HTMLAudioElement = new Audio("/AudioFiles/Future/FutureFIght.wav");
-    const futureGenSpaceship1: HTMLAudioElement = new Audio("/AudioFiles/Future/Spaceship Flyby.wav");
-    const futureGenspaceship2: HTMLAudioElement = new Audio("/AudioFiles/Future/SpaceshipDeep.wav");
+    const futureLoopMusic: HTMLAudioElement = new Audio("AudioFiles/Future/MarketMusic_Future.wav");
+    const futureLoop: HTMLAudioElement = new Audio("AudioFiles/Future/FutureMain.wav");
+    const futureGenFight: HTMLAudioElement = new Audio("AudioFiles/Future/FutureFIght.wav");
+    const futureGenSpaceship1: HTMLAudioElement = new Audio("AudioFiles/Future/Spaceship Flyby.wav");
+    const futureGenspaceship2: HTMLAudioElement = new Audio("AudioFiles/Future/SpaceshipDeep.wav");
     
     // const futureAudio: AudioScene = new Future(futureLoopMusic, futureLoop, futureGenFight, futureGenSpaceship1, futureGenspaceship2);
     
